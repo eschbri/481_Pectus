276ac6fc2b47a0f6ca4e3eb638be0e013c8baf31
Brian Esch <eschbri@umich.edu>
2017-02-19 11:13:06 -0500

    Drawing a cube now

--- opengl/playground.cpp
+++ opengl/playground.cpp
@@ -18,14 +18,81 @@ using namespace glm;
 using namespace std;
 
 static const GLfloat g_vertex_buffer_data[] = {
-	0.0f,  1.0f, 0.0f,
-	1.0f, 0.0f, 0.0f,
+	-1.0f,-1.0f,-1.0f, // triangle 1 : begin
+	-1.0f,-1.0f, 1.0f,
+	-1.0f, 1.0f, 1.0f, // triangle 1 : end
+	1.0f, 1.0f,-1.0f, // triangle 2 : begin
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f,-1.0f, // triangle 2 : end
+	1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	-1.0f,-1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f,-1.0f,
+	-1.0f, 1.0f,-1.0f,
+	1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f
+};
 
-	-1.0f, -1.0f, 0.0f,
-	0.0f, 1.0f, 0.0f,
-
-	1.0f, -1.0f, 0.0f,
-	0.0f, 0.0f, 1.0f,
+static const GLfloat g_color_buffer_data[] = {
+	0.583f,  0.771f,  0.014f,
+	0.609f,  0.115f,  0.436f,
+	0.327f,  0.483f,  0.844f,
+	0.822f,  0.569f,  0.201f,
+	0.435f,  0.602f,  0.223f,
+	0.310f,  0.747f,  0.185f,
+	0.597f,  0.770f,  0.761f,
+	0.559f,  0.436f,  0.730f,
+	0.359f,  0.583f,  0.152f,
+	0.483f,  0.596f,  0.789f,
+	0.559f,  0.861f,  0.639f,
+	0.195f,  0.548f,  0.859f,
+	0.014f,  0.184f,  0.576f,
+	0.771f,  0.328f,  0.970f,
+	0.406f,  0.615f,  0.116f,
+	0.676f,  0.977f,  0.133f,
+	0.971f,  0.572f,  0.833f,
+	0.140f,  0.616f,  0.489f,
+	0.997f,  0.513f,  0.064f,
+	0.945f,  0.719f,  0.592f,
+	0.543f,  0.021f,  0.978f,
+	0.279f,  0.317f,  0.505f,
+	0.167f,  0.620f,  0.077f,
+	0.347f,  0.857f,  0.137f,
+	0.055f,  0.953f,  0.042f,
+	0.714f,  0.505f,  0.345f,
+	0.783f,  0.290f,  0.734f,
+	0.722f,  0.645f,  0.174f,
+	0.302f,  0.455f,  0.848f,
+	0.225f,  0.587f,  0.040f,
+	0.517f,  0.713f,  0.338f,
+	0.053f,  0.959f,  0.120f,
+	0.393f,  0.621f,  0.362f,
+	0.673f,  0.211f,  0.457f,
+	0.820f,  0.883f,  0.371f,
+	0.982f,  0.099f,  0.879f
 };
 
 int main(){
@@ -59,13 +126,19 @@ int main(){
 	// Add vertex attribute to the vertex buffer array
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(
-		0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-		3,                  // size
-		GL_FLOAT,           // type
-		GL_FALSE,           // normalized?
-		sizeof(float) * 6,                  // stride
-		(void*)0            // array buffer offset
-	);
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(char*)0            // array buffer offset
+			);
+
+	// Create buffer and bind buffer for color array
+	GLuint colorbuffId;
+	glGenBuffers(1, &colorbuffId);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffId);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(
@@ -73,8 +146,8 @@ int main(){
 			3,
 			GL_FLOAT,
 			GL_FALSE,
-			sizeof(float) * 6,
-			(char*)(sizeof(float) * 3)
+			0,
+			(char*)0
 			);
 
 	// Create a matrix for transformations
@@ -84,15 +157,19 @@ int main(){
 			glm::vec3(0, 0, 0), // and looks at the origin
 			glm::vec3(0, 1, 0)); // Head is up (set to 0, -1, 0 to look upside down
 	glm::mat4 projection = glm::perspective(45.0f / 4.0f, 4.0f / 3.0f, 0.1f, 100.0f);
-	glm::mat4 mvp = projection * view * model;
+	glm::mat4 mvp = projection * view * model; // Order goes projection * (view * model)
 	glUniformMatrix4fv(matrixId, 1, GL_FALSE, &mvp[0][0]);
+
+	// Enable depth test
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
 
 	while(!glfwWindow.closed()){
 		// Clear screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		glDrawArrays(GL_TRIANGLES, 0, 12 * 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
 
 		// Swap buffers
 		glfwWindow.swapBuffers();
