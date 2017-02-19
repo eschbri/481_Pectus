feb588abc681ca4a7ed51e6a83624ee0f01edd23
Brian Esch <eschbri@umich.edu>
2017-02-19 16:49:00 -0500

    Added updates to read obj files in common

--- opengl/common.h
+++ opengl/common.h
@@ -2,6 +2,15 @@
 #define COMMON_H
 
 #include <GL/glew.h>
+#include <vector>
+#include <glm/glm.hpp>
+
+bool loadOBJ(
+	const char * path, 
+	std::vector<glm::vec3> & out_vertices, 
+	std::vector<glm::vec2> & out_uvs, 
+	std::vector<glm::vec3> & out_normals
+);
 
 GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path);
 
